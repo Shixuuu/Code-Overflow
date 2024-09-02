9eefed4c6b30bc7f8d0a1b8e42f0f695fe3e3a8f
daveningfr <davenheng1234234@gmail.com>
2024-09-02 22:22:01 +0800

    sign up into database works

--- app/config.py
+++ app/config.py
@@ -1,7 +1,19 @@
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+WTF_CSRF_ENABLED = True
 
 class Config:
     # Base configuration class
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = 'supersecretkey'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+app = Flask(__name__)
+
+# Configuration of the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # Using SQLite as an example
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,38 @@
 from app import app
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for,flash
 from app.models import User, db
+from app.form import RegistrationForm, LoginForm
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    # Default form type
+    form_type = request.args.get('form_type', 'login')
+    
+    if form_type == 'signup':
+        form = RegistrationForm()
+    else:
+        form = LoginForm()
+    
+    return render_template('home.html', form_type=form_type, form=form)
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegistrationForm()
+    print("Form method:", request.method)  # Debugging line
+    if form.validate_on_submit():
+        print("Form is valid")  # Debugging line
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None:
+            new_user = User(username=form.username.data, email=form.email.data)
+            new_user.set_password(form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Congratulations, you are now registered!')
+            return redirect(url_for('home', form_type='login'))
+        else:
+            flash('User already exists. Please login.')
+            return redirect(url_for('home', form_type='login'))
+    else:
+        print("Form errors:", form.errors)  # Debugging line
+    return render_template('home.html', form=form)
